e2573bfa8c754ca32f5cba1e434366ea4996be66
sacazidal <tw1nkle001@yandex.ru>
2025-03-16 23:44:47 +0700

    обновил middleware, если токен есть, то не может перейти на auth, если наоборот, то на приват страницы

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,35 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { validPaths } from "./lib/paths";
+import { publicPaths, validPaths } from "./lib/paths";
 
 export function middleware(request: NextRequest) {
-  // Получаем текущий путь
   const path = request.nextUrl.pathname;
 
   if (path.startsWith("/_next/") || path.startsWith("/api/")) {
     return NextResponse.next();
   }
 
-  // Получаем куки
   const cookie = request.cookies.get("last-visited-path")?.value;
 
-  // Обновляем куки с текущим путем
   const response = NextResponse.next();
   response.cookies.set("last-visited-path", path);
 
-  // Проверяем, существует ли запрашиваемый путь
-  const isPathExists = checkIfPathExists(path); // Реализуйте эту функцию
+  const isPathExists = checkIfPathExists(path);
 
   if (!isPathExists) {
-    // Если путь не существует, перенаправляем на последний сохраненный путь из куки
-    const redirectPath = cookie || "/"; // Если куки нет, перенаправляем на главную
+    const redirectPath = cookie || "/";
+    return NextResponse.redirect(new URL(redirectPath, request.url));
+  }
+
+  const token = request.cookies.get("token")?.value;
+
+  if (!token && !publicPaths.includes(path)) {
+    const redirectPath = token || "/login";
+    return NextResponse.redirect(new URL(redirectPath, request.url));
+  }
+
+  if (token && publicPaths.includes(path)) {
+    const redirectPath = "/";
     return NextResponse.redirect(new URL(redirectPath, request.url));
   }
 
